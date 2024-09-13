2edb4e872faac5baa46806202969027a7eb4048a
DhirenTheHeadlights <dhirenj03@gmail.com>
2024-09-13 11:55:46 -0500

    Refactor collision handling and add physics components
    Updated `CollisionHandler.h` to include `<algorithm>` and undefine `min` and `max` to prevent conflicts with the Windows API. Modified `checkCollision` method for `BoundingBox` objects to use references and correct logical conditions. Refactored `calculateCollisionInformation` method for simpler penetration depth and collision normal calculation. Updated `setCollisionInformation` to accept non-const references to `BoundingBox` objects. Changed `addObject` and `removeObject` methods to work with pointers. Refactored `update` method in `CollisionHandler` for efficiency.
    
    Added `System.h` with `Physics` namespace and `updateEntities` declaration. Added `System.cpp` to define `updateEntities` function. Updated `Arena::render` to change `drawBoundingBox` call. Modified `Player::initialize` for initial bounding box position and size. Uncommented line in `Player::update` to move player back on collision. Updated `Player::render` to draw bounding box as wireframe.
    
    Added `Component.h` defining `Component` struct in `Engine::Physics` namespace for physics properties.

--- src/Game/Player.cpp
+++ src/Game/Player.cpp
@@ -3,7 +3,7 @@
 using namespace Game;
 
 void Player::initialize() {
-	boundingBoxes.push_back(Engine::BoundingBox({ 0.f, 0.f, 0.f }, 1.f, 1.f, 1.f));
+	boundingBoxes.push_back(Engine::BoundingBox({ -10.f, -10.f, -10.f }, 10.f, 10.f, 10.f));
 
 	movementKeys.insert({ GLFW_KEY_W, {0, 0, 1} });
 	movementKeys.insert({ GLFW_KEY_S, {0, 0, -1} });
@@ -27,7 +27,7 @@ void Player::update(const float deltaTime) {
 	// If the player is colliding with something, move the player back to the previous position
 	for (auto& bb : boundingBoxes) {
 		if (bb.collisionInformation.penetration > 0) {
-			//bb.move(-bb.collisionInformation.collisionNormal, bb.collisionInformation.penetration, deltaTime);
+			bb.move(-bb.collisionInformation.collisionNormal, bb.collisionInformation.penetration, deltaTime);
 		}
 	}
 	
@@ -38,6 +38,6 @@ void Player::update(const float deltaTime) {
 
 void Player::render(const glm::mat4& view, const glm::mat4& projection) {
 	for (auto& bb : boundingBoxes) {
-		drawBoundingBox(bb, view * projection, false);
+		drawBoundingBox(bb, view * projection, true);
 	}
 }
