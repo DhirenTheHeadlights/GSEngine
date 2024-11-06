32277ec6d908e38da0bcc264a3ae7aa9dc2ef95d
DhirenTheHeadlights <dhirenj03@gmail.com>
2024-11-06 14:05:14 -0600

    Refactor rendering interface management and ImGui setup
    Refactored rendering interface management from `SceneHandler` to `Window` module to improve modularity and maintainability. Standardized resource path definitions in `CMakeLists.txt` and updated ImGui setup and usage across multiple files.
    
    - Updated `CMakeLists.txt` to modify resource paths and compilation arguments, setting `IMGUI` macro to `0` by default.
    - Added `initialize()` and `exit()` functions in `Editor.h`, and changed `RenderingInterface` inheritance.
    - Modified `Editor.cpp` to set up and clean up ImGui, manage FBO binding, and integrate `renderImGuiViewport()` into `render()`.
    - Replaced `Engine::sceneHandler.addRenderingInterface` with `Engine::Window::addRenderingInterface` in `main.cpp`.
    - Removed `RenderingInterface` struct from `SceneHandler.h` and moved it to `Window.h`.
    - Added new functions and necessary includes in `Debug.h`.
    - Implemented rendering interface management in `Window.cpp` and updated frame functions.
    - Updated `EngineCore.cpp` for conditional ImGui compilation and modified `update()` and `render()` functions.
    - Removed rendering interface management from `SceneHandler.cpp`.
    - Implemented `setImguiSaveFilePath()` in `Debug.cpp` and updated ImGui state save/load functionality.
    - Commented out debug menu and window creation calls in `Arena.cpp` and `Game.cpp`.
    - Modified `imgui_state.json` file.

--- Editor/Source/main.cpp
+++ Editor/Source/main.cpp
@@ -2,11 +2,10 @@
 #include "Engine.h"
 #include "Game.h"
 
-
 int main() {
 	Engine::initialize(Game::initialize, Game::close);
 
-	Engine::sceneHandler.addRenderingInterface(std::make_shared<Editor::RenderingInterface>());
+	Engine::Window::addRenderingInterface(std::make_shared<Editor::RenderingInterface>());
 
 	Engine::Window::setFullScreen(false);
 	Game::setResizingEnabled(false);
