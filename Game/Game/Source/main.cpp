8c40c05ff542811df9569fd2bfec46d740105394
DhirenTheHeadlights <dhirenj03@gmail.com>
2024-11-10 16:15:03 -0600

    Exclude libraries from builds, add MSVC options, update enet
    Excluded GoonSquadLib, glfw, glad, enet, and glm from all builds.
    Added MSVC-specific compile options for Editor and Engine targets,
    including runtime library settings and AVX2 SIMD optimizations.
    Added options to disable building documentation, examples, and tests
    for GLFW. Updated FetchContent_Declare for enet to ensure the correct
    version tag is used. Modified add_library command for enet_static to
    include EXCLUDE_FROM_ALL property. Defined macros for IMGUI based on
    build type for both Editor and Engine targets. Added call to
    Engine::Debug::setImguiSaveFilePath in main.cpp to set ImGui state
    save file path. Added new imgui_state.json file to store ImGui window
    positions and sizes.

--- Game/Game/Source/main.cpp
+++ Game/Game/Source/main.cpp
@@ -2,6 +2,7 @@
 #include "Game.h"
 
 int main() {
+	Engine::Debug::setImguiSaveFilePath(RESOURCES_PATH "imgui_state.json");
 	Engine::initialize(Game::initialize, Game::close);
 	Engine::run(Game::update, Game::render);
 	return 0;
