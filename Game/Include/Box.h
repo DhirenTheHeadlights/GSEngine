9c6c490230d4dda348b9a63fe9cda8912af200cb
DhirenTheHeadlights <dhirenj03@gmail.com>
2024-11-11 09:59:12 -0600

    Modified box to take in a position. Changed the box added to the game to be in a position to interact more with the lighting.

--- Game/Include/Box.h
+++ Game/Include/Box.h
@@ -4,7 +4,8 @@
 namespace Game {
 	class Box : public Engine::Object {
 	public:
-		Box(const Engine::Vec3<Engine::Length>& dimensions) : Object(Engine::idHandler.generateID("Box")), dimensions(dimensions) {}
+		Box(const Engine::Vec3<Engine::Length>& position, const Engine::Vec3<Engine::Length>& size) 
+			: Object(Engine::idHandler.generateID("Box")), position(position), size(size) {}
 		~Box() = default;
 
 		void initialize();
@@ -12,6 +13,7 @@ namespace Game {
 		void render() override;
 
 	private:
-		Engine::Vec3<Engine::Length> dimensions;
+		Engine::Vec3<Engine::Length> position;
+		Engine::Vec3<Engine::Length> size;
 	};
 }
