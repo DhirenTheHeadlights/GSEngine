0714ca5d71facf29d3b329546c88cb91f95e723e
DhirenTheHeadlights <dhirenj03@gmail.com>
2024-11-20 13:58:39 -0600

    Added a sphere entity to actually have a mesh for the arena lighting. The next step would be a bloom effect. Moved the box file into the engine as a base example object.

---
+++ Engine/Engine/Include/Core/Object/Box.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Engine.h"
+
+namespace Engine {
+	class Box : public Object {
+	public:
+		Box(const Vec3<Length>& position, const Vec3<Length>& size) 
+			: Object("Box"), position(position), size(size) {}
+
+		void initialize();
+		void update() override {}
+		void render() override {}
+
+	private:
+		Vec3<Length> position;
+		Vec3<Length> size;
+	};
+}
