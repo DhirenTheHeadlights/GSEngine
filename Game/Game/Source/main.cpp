e94aa406636527322e4c72cae329245a16036e2b
DhirenTheHeadlights <dhirenj03@gmail.com>
2025-02-21 15:05:35 -0600

    compiles but vulkan is not happy with the descriptor sets

--- Game/Game/Source/main.cpp
+++ Game/Game/Source/main.cpp
@@ -2,8 +2,9 @@ import gse;
 import game;
 
 auto main() -> int {
-	gse::set_imgui_enabled(true);
+	/*gse::set_imgui_enabled(true);
 	gse::debug::set_imgui_save_file_path(game::config::resource_path / "imgui_state.ini");
 	gse::initialize(game::initialize, game::close);
-	gse::run(game::update, game::render);
+	gse::run(game::update, game::render);*/
+	gse::window::initialize_vk();
 }
