b8e2f854b5af2a71f43f8b7ecc178ad0eeb7231b
DhirenTheHeadlights <dhirenj03@gmail.com>
2025-04-24 18:41:51 -0500

    mroe work on transient allocator

--- Engine/Engine/Source/Platform/Vulkan/TransientAllocator.cpp
+++ Engine/Engine/Source/Platform/Vulkan/TransientAllocator.cpp
@@ -1,1 +1,19 @@
 module gse.vulkan.transient_allocator;
+
+import std;
+import vulkan_hpp;
+
+import gse.platform.context;
+import gse.platform.assert;
+
+struct memory_block {
+	vk::DeviceMemory memory;
+	vk::DeviceSize size, cursor = 0;
+	vk::MemoryPropertyFlags properties;
+	void* mapped = nullptr;
+};
+
+struct pool {
+	std::uint32_t memory_type_index;
+	std::vector<memory_block> blocks;
+};
